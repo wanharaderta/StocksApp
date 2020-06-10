45778fd6041c3759df7a1a614a82fd213b64a68b
wanhar <wanhar@sehatq.com>
2020-06-10 21:39:35 +0700

    Initial Commit

---
+++ StocksApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StocksApp
+//
+//  Created by Wanhar on 10/06/20.
+//  Copyright © 2020 Wanhar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
