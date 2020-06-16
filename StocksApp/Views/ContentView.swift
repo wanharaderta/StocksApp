bbd0c6778e437fb86c85cd818705073e061d4377
wanhar <wanhar@sehatq.com>
2020-06-16 23:50:12 +0700

    Adding Stock List

--- StocksApp/Views/ContentView.swift
+++ StocksApp/Views/ContentView.swift
@@ -9,16 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject private var stockListVM = StockListViewModel()
+    
+    
+    init() {
+        UINavigationBar.appearance().backgroundColor    = UIColor.black
+        UINavigationBar.appearance().largeTitleTextAttributes   = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        UITableView.appearance().backgroundColor    = UIColor.black
+        UITableViewCell.appearance().backgroundColor    = UIColor.black
+        
+        stockListVM.load()
+    }
+    
     var body: some View {
         NavigationView {
             
-            ZStack {
+            ZStack(alignment: .leading) {
                 Color.black
-                Text("ddd")
+                
+                Text("January 5 2020")
+                    .font(.custom("Arial", size: 32))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.gray)
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+                    .offset(y: -400)
+                
+                SearchView(searchTerm: self.$stockListVM.searchTerm)
+                    .offset(y: -350 )
+                
+                StockListView(stocks: self.stockListVM.stocks)
+                    .offset(y: 110)
+                
             }
-            
-        .navigationBarTitle("Stocks")
-        }
+                
+            .navigationBarTitle("Stocks")
+        }.edgesIgnoringSafeArea(Edge.Set(.bottom))
     }
 }
 
